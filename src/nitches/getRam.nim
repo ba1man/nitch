ac191829c6c29d62c91f835e6a8c2c35d16973c6
ba1man <1152575283@qq.com>
2024-02-13 21:00:25 +0800

    update getRam

--- src/nitches/getRam.nim
+++ src/nitches/getRam.nim
@@ -13,14 +13,14 @@ proc getRam*(): string =
   let sysinfo = strip(execProcess("sysctl -n hw.pagesize hw.memsize vm.page_pageable_internal_count vm.page_purgeable_count")).split("\n")
   let hw_pagesize = sysinfo[0].parseUInt
   let mem_total = sysinfo[1].parseUInt div 1024 div 1024
-  # let pages_app = sysinfo[2].parseUInt - sysinfo[3].parseUInt
+  let pages_app = sysinfo[2].parseUInt - sysinfo[3].parseUInt
 
   let vminfo = strip(execProcess("vm_stat"))
   let pages_wired = parseVMStat(vminfo, "Pages wired down").parseUInt
   let pages_compressed = parseVMStat(vminfo, "Pages occupied by compressor").parseUInt
-  let pages_active = parseVMStat(vminfo, "Pages active").parseUInt
+  # let pages_active = parseVMStat(vminfo, "Pages active").parseUInt
 
-  let mem_used = (pages_active + pages_wired + pages_compressed) * hw_pagesize div 1024 div 1024
+  let mem_used = (pages_app + pages_wired + pages_compressed) * hw_pagesize div 1024 div 1024
   let mem_label = "MiB"
 
   result = &"{uint(mem_used)}{mem_label} / {uint(mem_total)}{mem_label}"
